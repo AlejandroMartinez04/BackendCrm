911dbd27a9136e178a38ef4482604ea84ed80215
DiegoM <diegomartinez1101@gmail.com>
2023-04-11 21:40:56 -0500

    Config cors for everyone

--- src/index.js
+++ src/index.js
@@ -10,14 +10,9 @@ const casesRoutes = require("./routes/cases");
 const personRoutes = require("./routes/person");
 const employeeRoutes = require("./routes/employee");
 
-// const whiteList = ['https://sadimi-suj6.onrender.com','http://localhost:3000'];
+//const whiteList = ['https://sadimi-suj6.onrender.com'];
 
-// app.use(cors({origin: whiteList }));
-
-var corsOptions = {
-    origin: 'http://10.223.152.132',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+app.use(cors()); // Permite todas las conexiones
 
 //Settings
 app.set('port', process.env.PORT || 3000);
@@ -28,23 +23,21 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(bodyParser.json()); 
 
-//Routes
+//routes
 app.use('/api', userRoutes);
 app.use('/api', casesRoutes);
 app.use('/api', personRoutes);
 app.use('/api', employeeRoutes);
 
 
-app.get("/", cors(corsOptions), function (req, res, next) {
+app.get("/", (req,res) => {
     res.send("Welcome to sadimi");
 });
-
-
 
 //Static files
 app.use(express.static(path.join(__dirname, 'public')))
 
 //Starting server
-app.listen(10000, function () {
+app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
 });
